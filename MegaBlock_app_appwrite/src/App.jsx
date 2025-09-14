84ea963cea2fc668576734ea0c2346ed14b796ee
Shivam Gaur <shivamgaur865029@gmail.com>
2025-09-14 13:45:19 +0530

    basic_appwriteisdone

---
+++ MegaBlock_app_appwrite/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import conf from './conf/conf';
+
+function App() {
+  // console.log(conf.appwriteUrl);
+  // console.log(conf.appwriteProjectId);
+  // console.log(conf.appwriteDatabaseId);
+  // console.log(conf.appwriteCollectionId);
+  // console.log(conf.appwriteBucketId);
+ 
+
+  return (
+    <>
+        <h1> A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
